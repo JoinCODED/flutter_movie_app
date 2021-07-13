5b9d17c4a095a1c4962d848a2d3bd34a1c329e43
Dhari <dmakwt@gmail.com>
2021-07-13 12:41:09 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Moive App',
+      theme: ThemeData(
+        primaryColor: Colors.redAccent,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List movies = [
+    'Toy Story',
+    'Spider Man',
+    'Inside out',
+    'The LEGO Movie',
+    'The Lion King',
+    'Frozen',
+    'Shrek',
+    'BIG HERO',
+    'ICE AGE',
+    'BRAVE',
+  ];
+
+  List imgPaths = [
+    'assets/toy_story.jpeg',
+    'assets/spider_man.jpeg',
+    'assets/inside_out.jpeg',
+    'assets/lego.jpeg',
+    'assets/lion_king.jpeg',
+    'assets/frozen.jpeg',
+    'assets/shrek.jpeg',
+    'assets/big_hero.jpeg',
+    'assets/ice_age.jpeg',
+    'assets/brave.jpeg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Movie APP',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      movies[index],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Image.asset(
+                    imgPaths[index],
+                    width: 125,
+                    height: 125,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
