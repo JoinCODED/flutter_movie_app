167172c3f45c7aac12ec5f80f798efb109233955
yanal shmilan <yshmilan@gmail.com>
2023-02-13 07:18:17 +0300

    Revert "start point"
    This reverts commit a4cc4a9c8baf2ff4106bdd63588c219c98285334.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/movie.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,43 +26,73 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List movies = [
-    'Toy Story',
-    'Spider Man',
-    'Inside out',
-    'The LEGO Movie',
-    'The Lion King',
-    'Frozen',
-    'Shrek',
-    'BIG HERO',
-    'ICE AGE',
-    'BRAVE',
-  ];
-
-  List imgPaths = [
-    'assets/toy_story.jpeg',
-    'assets/spider_man.jpeg',
-    'assets/inside_out.jpeg',
-    'assets/lego.jpeg',
-    'assets/lion_king.jpeg',
-    'assets/frozen.jpeg',
-    'assets/shrek.jpeg',
-    'assets/big_hero.jpeg',
-    'assets/ice_age.jpeg',
-    'assets/brave.jpeg',
+    Movie(name: 'Toy Story', imgPath: 'assets/toy_story.jpeg', rating: 8),
+    Movie(name: 'Spider Man', imgPath: 'assets/spider_man.jpeg', rating: 5),
+    Movie(name: 'Inside out', imgPath: 'assets/inside_out.jpeg', rating: 7),
+    Movie(name: 'The LEGO Movie', imgPath: 'assets/lego.jpeg', rating: 5.5),
+    Movie(name: 'The Lion King', imgPath: 'assets/lion_king.jpeg', rating: 9),
+    Movie(name: 'Frozen', imgPath: 'assets/frozen.jpeg', rating: 4.5),
+    Movie(name: 'Shrek', imgPath: 'assets/shrek.jpeg', rating: 8.5),
+    Movie(name: 'BIG HERO', imgPath: 'assets/big_hero.jpeg', rating: 8),
+    Movie(name: 'ICE AGE', imgPath: 'assets/ice_age.jpeg', rating: 7.5),
+    Movie(name: 'BRAVE', imgPath: 'assets/brave.jpeg', rating: 7),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Movie APP',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
+      appBar: AppBar(
+        title: Text(
+          'Movie APP',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
           ),
         ),
-        // TODO: #1 Create movie tile
-        body: null);
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          '${movies[index].name}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Container(height: 10),
+                        Text(
+                          'Rating: ${movies[index].rating}/10',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey[700],
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Image.asset(
+                    movies[index].imgPath,
+                    width: 125,
+                    height: 125,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
